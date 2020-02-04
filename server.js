e3ad6de9c75db93bf8680e9eb5affc235856724a
Cao Mai <caomai@rocketmail.com>
2020-02-03 16:37:34 -0800

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 const app = express()
 const port = 3000
 
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+require('./controllers/posts.js')(app);
 
 // Middleware
 
@@ -14,13 +26,7 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.render('home'))
 
-
-app.get('/greetings/:name', (req, res) => {
-    // grab the name from the path provided
-    const name = req.params.name;
-    // render the greetings view, passing along the name
-    res.render('main', { name });
-  })
+app.get('/posts/new', (req, res) => res.render('posts-new'))
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
